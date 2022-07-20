6d51384bd5a7031cfe913e1a00e63102fc925fa9
XYJ <XYJ@XYJ.com>
2022-07-20 21:20:53 +0800

    second commit

--- src/gitdemo1/gitdemoclass.java
+++ src/gitdemo1/gitdemoclass.java
@@ -3,5 +3,6 @@ package gitdemo1;
 public class gitdemoclass {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("hello world2");
     }
 }
